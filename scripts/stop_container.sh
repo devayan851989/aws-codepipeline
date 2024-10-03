aad91e763a478c38b94a7ef9ff46b25aefc718ef
devayan851989 <devayanthakur@gmail.com>
2024-10-03 20:23:21 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,4 +4,4 @@ set -e
 # Stop the running container (if any)
 containerid= sudo docker container ls  | grep 'devayanthakur/simple-python-flask-ap' | awk '{print $1}'
 echo "containerid : $containerid"
-docker rm -f $containerid
+sudo docker rm -f $containerid
